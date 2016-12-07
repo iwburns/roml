ef711032690522e72e1218afefc7e306136b7038
Ian Burns <iwburns8@gmail.com>
2016-12-06 23:18:44 -0600

    make add generic over the argument type to emulate method overloading
    very much a WiP.

--- src/matrix/mod.rs
+++ src/matrix/mod.rs
@@ -1,36 +1,36 @@
-use super::vector::Vector3;
-
-// Self just needs to be a matrix.  Size and shape of Self do not matter.
-pub trait Matrix<T> {
-    fn add(&mut self, m: &Self) -> &mut Self;
-    fn add_into(&self, m: &Self, dest: &mut Self);
-    fn sub(&mut self, m: &Self) -> &mut Self;
-    fn sub_into(&self, m: &Self, dest: &mut Self);
-    //todo: add more here
-}
-
-// Self must be a Square Matrix, but these functions cannot depend upon the size of Self.
-pub trait SquareMatrix<T> : Matrix<T> {
-    fn determinant(&self) -> T;
-    fn identity(&mut self) -> &mut Self;
-    fn invert(&mut self) -> &mut Self;
-    fn invert_into(&self, dest: &mut Self);
-    //todo: add more here
-}
-
-pub enum Mat3Index {
-    One,
-    Two,
-    Three,
-}
-
-// Self has to be a square 3x3 matrix
-pub trait Matrix3<T> : SquareMatrix<T> {
-    type V: Vector3<T>;
-
-    fn get_column(&self, column: Mat3Index, dest: &mut Self::V);
-    fn get_row(&self, row: Mat3Index, dest: &mut Self::V);
-}
-
-
-pub mod matrix3f;
+//use super::vector::Vector3;
+//
+//// Self just needs to be a matrix.  Size and shape of Self do not matter.
+//pub trait Matrix<T> {
+//    fn add(&mut self, m: &Self) -> &mut Self;
+//    fn add_into(&self, m: &Self, dest: &mut Self);
+//    fn sub(&mut self, m: &Self) -> &mut Self;
+//    fn sub_into(&self, m: &Self, dest: &mut Self);
+//    //todo: add more here
+//}
+//
+//// Self must be a Square Matrix, but these functions cannot depend upon the size of Self.
+//pub trait SquareMatrix<T> : Matrix<T> {
+//    fn determinant(&self) -> T;
+//    fn identity(&mut self) -> &mut Self;
+//    fn invert(&mut self) -> &mut Self;
+//    fn invert_into(&self, dest: &mut Self);
+//    //todo: add more here
+//}
+//
+//pub enum Mat3Index {
+//    One,
+//    Two,
+//    Three,
+//}
+//
+//// Self has to be a square 3x3 matrix
+//pub trait Matrix3<T> : SquareMatrix<T> {
+//    type V: Vector3<T>;
+//
+//    fn get_column(&self, column: Mat3Index, dest: &mut Self::V);
+//    fn get_row(&self, row: Mat3Index, dest: &mut Self::V);
+//}
+//
+//
+//pub mod matrix3f;
--- src/vector/vector3f.rs
+++ src/vector/vector3f.rs
@@ -1,136 +1,136 @@
-use super::Vector;
-use super::Vector3;
-
-pub struct Vector3f {
-    pub x: f32,
-    pub y: f32,
-    pub z: f32,
-}
-
-impl Vector<f32> for Vector3f {
-    fn add(&mut self, v: &Vector3f) -> &mut Vector3f {
-        unimplemented!()
-    }
-
-    fn add_into(&self, v: &Vector3f, dest: &mut Vector3f) {
-        unimplemented!()
-    }
-
-    fn angle(&self, v: &Vector3f) -> f32 {
-        unimplemented!()
-    }
-
-    fn angle_cos(&self, v: &Vector3f) -> f32 {
-        unimplemented!()
-    }
-
-    fn distance(&self, v: &Vector3f) -> f32 {
-        unimplemented!()
-    }
-
-    fn distance_squared(&self, v: &Vector3f) -> f32 {
-        unimplemented!()
-    }
-
-    fn dot(&self, v: &Vector3f) -> f32 {
-        unimplemented!()
-    }
-
-    fn fma_scalar(&mut self, a: f32, b: &Vector3f) -> &mut Vector3f {
-        unimplemented!()
-    }
-
-    fn fma_scalar_into(&self, a: f32, b: &Vector3f, dest: &mut Vector3f) {
-        unimplemented!()
-    }
-
-    fn fma_vector(&mut self, a: &Vector3f, b: &Vector3f) -> &mut Vector3f {
-        unimplemented!()
-    }
-
-    fn fma_vector_into(&self, a: &Vector3f, b: &Vector3f, dest: &mut Vector3f) {
-        unimplemented!()
-    }
-
-    fn length(&self) -> f32 {
-        unimplemented!()
-    }
-
-    fn length_squared(&self) -> f32 {
-        unimplemented!()
-    }
-
-    fn lerp(&mut self, other: &Vector3f, t: f32) -> &mut Vector3f {
-        unimplemented!()
-    }
-
-    fn lerp_into(&self, other: &Vector3f, t: f32, dest: &mut Vector3f) {
-        unimplemented!()
-    }
-
-    fn mul_scalar(&mut self, s: f32) -> &mut Vector3f {
-        unimplemented!()
-    }
-
-    fn mul_scalar_into(&self, s: f32, dest: &mut Vector3f) {
-        unimplemented!()
-    }
-
-    fn mul_vector(&mut self, v: &Vector3f) -> &mut Vector3f {
-        unimplemented!()
-    }
-
-    fn mul_vector_into(&self, v: &Vector3f, dest: &mut Vector3f) {
-        unimplemented!()
-    }
-
-    fn negate(&mut self) -> &mut Vector3f {
-        unimplemented!()
-    }
-
-    fn negate_into(&self, dest: &mut Vector3f) {
-        unimplemented!()
-    }
-
-    fn normalize(&mut self) -> &mut Vector3f {
-        unimplemented!()
-    }
-
-    fn normalize_into(&self, dest: &mut Vector3f) {
-        unimplemented!()
-    }
-
-    fn set(&mut self, v: &Vector3f) -> &mut Vector3f {
-        unimplemented!()
-    }
-
-    fn sub(&mut self, v: &Vector3f) -> &mut Vector3f {
-        unimplemented!()
-    }
-
-    fn sub_into(&self, v: &Vector3f, dest: &mut Vector3f) {
-        unimplemented!()
-    }
-
-    fn zero(&mut self) -> &mut Vector3f {
-        unimplemented!()
-    }
-}
-
-impl Vector3<f32> for Vector3f {
-    fn add_components(&mut self, x: f32, y: f32, z: f32) -> &mut Vector3f {
-        unimplemented!()
-    }
-
-    fn add_components_into(&self, x: f32, y: f32, z: f32, dest: &mut Vector3f) {
-        unimplemented!()
-    }
-
-    fn cross(&mut self, v: &Vector3f) -> &mut Vector3f {
-        unimplemented!()
-    }
-
-    fn cross_into(&self, v: &Vector3f, dest: &mut Vector3f) {
-        unimplemented!()
-    }
-}
+//use super::Vector;
+//use super::Vector3;
+//
+//pub struct Vector3f {
+//    pub x: f32,
+//    pub y: f32,
+//    pub z: f32,
+//}
+//
+//impl Vector<f32> for Vector3f {
+//    fn add(&mut self, v: &Vector3f) -> &mut Vector3f {
+//        unimplemented!()
+//    }
+//
+//    fn add_into(&self, v: &Vector3f, dest: &mut Vector3f) {
+//        unimplemented!()
+//    }
+//
+//    fn angle(&self, v: &Vector3f) -> f32 {
+//        unimplemented!()
+//    }
+//
+//    fn angle_cos(&self, v: &Vector3f) -> f32 {
+//        unimplemented!()
+//    }
+//
+//    fn distance(&self, v: &Vector3f) -> f32 {
+//        unimplemented!()
+//    }
+//
+//    fn distance_squared(&self, v: &Vector3f) -> f32 {
+//        unimplemented!()
+//    }
+//
+//    fn dot(&self, v: &Vector3f) -> f32 {
+//        unimplemented!()
+//    }
+//
+//    fn fma_scalar(&mut self, a: f32, b: &Vector3f) -> &mut Vector3f {
+//        unimplemented!()
+//    }
+//
+//    fn fma_scalar_into(&self, a: f32, b: &Vector3f, dest: &mut Vector3f) {
+//        unimplemented!()
+//    }
+//
+//    fn fma_vector(&mut self, a: &Vector3f, b: &Vector3f) -> &mut Vector3f {
+//        unimplemented!()
+//    }
+//
+//    fn fma_vector_into(&self, a: &Vector3f, b: &Vector3f, dest: &mut Vector3f) {
+//        unimplemented!()
+//    }
+//
+//    fn length(&self) -> f32 {
+//        unimplemented!()
+//    }
+//
+//    fn length_squared(&self) -> f32 {
+//        unimplemented!()
+//    }
+//
+//    fn lerp(&mut self, other: &Vector3f, t: f32) -> &mut Vector3f {
+//        unimplemented!()
+//    }
+//
+//    fn lerp_into(&self, other: &Vector3f, t: f32, dest: &mut Vector3f) {
+//        unimplemented!()
+//    }
+//
+//    fn mul_scalar(&mut self, s: f32) -> &mut Vector3f {
+//        unimplemented!()
+//    }
+//
+//    fn mul_scalar_into(&self, s: f32, dest: &mut Vector3f) {
+//        unimplemented!()
+//    }
+//
+//    fn mul_vector(&mut self, v: &Vector3f) -> &mut Vector3f {
+//        unimplemented!()
+//    }
+//
+//    fn mul_vector_into(&self, v: &Vector3f, dest: &mut Vector3f) {
+//        unimplemented!()
+//    }
+//
+//    fn negate(&mut self) -> &mut Vector3f {
+//        unimplemented!()
+//    }
+//
+//    fn negate_into(&self, dest: &mut Vector3f) {
+//        unimplemented!()
+//    }
+//
+//    fn normalize(&mut self) -> &mut Vector3f {
+//        unimplemented!()
+//    }
+//
+//    fn normalize_into(&self, dest: &mut Vector3f) {
+//        unimplemented!()
+//    }
+//
+//    fn set(&mut self, v: &Vector3f) -> &mut Vector3f {
+//        unimplemented!()
+//    }
+//
+//    fn sub(&mut self, v: &Vector3f) -> &mut Vector3f {
+//        unimplemented!()
+//    }
+//
+//    fn sub_into(&self, v: &Vector3f, dest: &mut Vector3f) {
+//        unimplemented!()
+//    }
+//
+//    fn zero(&mut self) -> &mut Vector3f {
+//        unimplemented!()
+//    }
+//}
+//
+//impl Vector3<f32> for Vector3f {
+//    fn add_components(&mut self, x: f32, y: f32, z: f32) -> &mut Vector3f {
+//        unimplemented!()
+//    }
+//
+//    fn add_components_into(&self, x: f32, y: f32, z: f32, dest: &mut Vector3f) {
+//        unimplemented!()
+//    }
+//
+//    fn cross(&mut self, v: &Vector3f) -> &mut Vector3f {
+//        unimplemented!()
+//    }
+//
+//    fn cross_into(&self, v: &Vector3f, dest: &mut Vector3f) {
+//        unimplemented!()
+//    }
+//}
